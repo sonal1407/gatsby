faccb331966c276b7cad867d26cdc8902d8f9044
sonalPrajapati <sonal.prajapati@1rivet.com>
2020-11-09 11:41:02 +0530

    content full api added to display the data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -11,6 +18,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
--- src/components/shared/backgroundImage.js
+++ src/components/shared/backgroundImage.js
@@ -17,3 +17,5 @@ export default function BackgroundImagePage({
         </div>
     )
 }
+
+// ShoppingItem
